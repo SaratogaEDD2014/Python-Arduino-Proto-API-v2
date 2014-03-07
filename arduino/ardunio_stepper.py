686439cdfbf723cddc8576096d3d1e8f9e6fb247
SkottyK <skrulcik@gmail.com>
2014-03-07 13:50:47 -0500

    Update ardunio_stepper.py

--- arduino/ardunio_stepper.py
+++ arduino/ardunio_stepper.py
@@ -2,7 +2,8 @@ from arduino import *
 import time
 
 class ArduinoStepMotor(Arduino):
-    def __init__(self, port, baudrate=115200, units = 'in', limit = 12):
+    def __init__(self, port, baudrate=9600, units = 'mm', limit = 12):
+        #115200
         super(ArduinoStepMotor, self).__init__(port, baudrate)
         self.units = units #whether we are using metric or English, important for calculating pitch
         self.position = 0 #the absolute linear position of the motor apparatus
@@ -11,17 +12,33 @@ class ArduinoStepMotor(Arduino):
         if(self.units == 'in'):
             self.pitch = 50.8
         elif(self.units == 'mm'):
-            self.pitch = 2
+            self.pitch = 2.
         else:
             print('invalid agruement for units, units must be set to "in" or "mm"')
             self.pitch = 50.8
+    def step_one(self):
+        self.sendData('5')
+        print self.getData()
 
     def relMove(self, distance):
-        if(distance < 900): #900 is a dummy value for testing, will switch to self.limit later
-            self.sendData('6')
-            self.sendData(str(distance))
-        else:
-            print("distance out of range")
+        distance = int(round(distance))
+        multiplier = -1 if distance<0 else 1
+        for i in range(abs(distance)/100):
+            self._send_steps(100*multiplier)
+        if abs(distance)%100 != 0:
+            self._send_steps((abs(distance)%100)*multiplier)
+
+    def move_mm(self, mm):
+        #2mm pitch
+        steps_per_rev = 9000
+        revs = (mm/self.pitch)
+        steps = revs*steps_per_rev
+        self.relMove(steps)
+
+    def _send_steps(self, steps):
+        self.sendData('6')
+        self.sendData(steps)
+        print self.getData()
 
     def absMove(self, newPosition):
         if (newPosition < 0):
@@ -34,8 +51,8 @@ class ArduinoStepMotor(Arduino):
     def reset(self): # WARNING: THIS CAN INTERFERE WITH PROTCOLS WHICH PREVENT THE MOTOR FROM MOVING OUT OF RANGE
         self.position = 0
 
-ar1 = ArduinoStepMotor('COM5')
-for i in range(200):
-    time.sleep(.005) #time.sleep is needed to prevent the arduino from crashing
-    ar1.relMove(400)
-
+sKotty = ArduinoStepMotor('COM5')
+sKotty.output([2,3])
+##for i in range(100):
+##    sKotty.relMove(-100)
+#sKotty.close()
